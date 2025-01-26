e5178a550893feef42e808abcfa44be096ff18c4
db-admin <70283578+lukestorm19@users.noreply.github.com>
2025-01-26 10:57:03 -0800

    LJ || adding search bar on top and testing api for search

--- routes/movies.js
+++ routes/movies.js
@@ -31,6 +31,18 @@ router.get("/movie/:id", async (req, res) => {
   }
 });
 
+router.get("/search", async (req, res) => {
+  const query = req.query.q;
+  try {
+    const response = await axios.get("https://api.themoviedb.org/3/search/movie", {
+      params: { query, api_key: TMDB_API_KEY },
+    });
+    res.render("pages/home", { movies: response.data.results });
+  } catch (error) {
+    console.error("Error fetching search results:", error.message);
+    res.status(500).send("Error fetching search results");
+  }
+});
 
 
 module.exports = router;
